40d52ac0193e107f7bb1453892ce7f4e8aca0c01
Takanari Hayama <taki@igel.co.jp>
2015-02-18 08:24:28 +0900

    compositor-drm: map framebuffers with read/write permission.
    We should map framebuffers with read and write permission. Otherwise, we
    won't be able to read back framebuffers when we capture frames.

--- libweston/compositor-drm.c
+++ libweston/compositor-drm.c
@@ -364,7 +364,7 @@ drm_fb_create_dumb(struct drm_backend *b, int width, int height,
 	if (ret)
 		goto err_add_fb;
 
-	fb->map = mmap(NULL, fb->size, PROT_WRITE,
+	fb->map = mmap(NULL, fb->size, PROT_READ | PROT_WRITE,
 		       MAP_SHARED, b->drm.fd, map_arg.offset);
 	if (fb->map == MAP_FAILED)
 		goto err_add_fb;
